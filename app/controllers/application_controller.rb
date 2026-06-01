e06f73a423d2f573d7be7f9ee06ca79226831d75
Achyuth <achyuth@spritle.com>
2026-06-01 18:31:13 +0530

    set cache headers added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :check_active!
+  before_action :set_cache_headers
   protect_from_forgery with: :exception
   helper_method :admin?, :log_activity
   private
@@ -18,5 +19,10 @@ class ApplicationController < ActionController::Base
     ActivityLog.create!(message: message, user: current_user)
   rescue => e
     Rails.logger.warn("Activity log error: #{e.message}")
+  end
+  def set_cache_headers
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
